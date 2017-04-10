232c1394333ecc62286ec610969909577ae541a2
junin17 <carlosarju@gmail.com>
2017-04-09 22:38:00 -0300

    post

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,7 @@ class NegociacaoController {
         this._inputQuantidade = elemento("#quantidade");
         this._inputValor = elemento
 
-        this._listaNegociacao = new Bind(new ListaNegociacao(), new NegociacaoView(elemento("#negociacoesView")), 'add', 'esvazia', 'addAll','orderBy');
+        this._listaNegociacao = new Bind(new ListaNegociacao(), new NegociacaoView(elemento("#negociacoesView")), 'add', 'esvazia', 'addAll', 'orderBy');
 
         this._mensagem = new Bind(new Mensagem(), new MensagemView(elemento("#mensagem")), 'texto');
 
@@ -47,18 +47,44 @@ class NegociacaoController {
         let service = new NegociacaoService();
 
         service.obterNegociacoes()
-        .then(negociacoes => {
-            let arrayNegociacoes = negociacoes.reduce((saida,atual) => saida.concat(atual),[]);
-            this._listaNegociacao.addAll(arrayNegociacoes);
-            this._mensagem.texto = "mensagens recebidas com sucesso";
-        }).catch(error => this._mensagem.texto = error);
+            .then(negociacoes => {
+                let arrayNegociacoes = negociacoes.reduce((saida, atual) => saida.concat(atual), []);
+                this._listaNegociacao.addAll(arrayNegociacoes);
+                this._mensagem.texto = "mensagens recebidas com sucesso";
+            }).catch(error => this._mensagem.texto = error);
 
-       
 
-     
+
+
     }
 
-    ordenaLista(prop){
+    ordenaLista(prop) {
         this._listaNegociacao.orderBy(prop);
+    }
+
+    sendPost(event) {
+        event.preventDefault();
+        console.log("Enviando post");
+        let selector = document.querySelector.bind(document);
+        inputData = selector('#data');
+        inputQuantidade = selector('#quantidade');
+        inputValor = selector('#valor');
+
+        let negociacao = {
+            data: inputData.value,
+            quantidade: inputQuantidade.value,
+            valor: inputValor.value
+        };
+
+        let http = new HttpService();
+
+        http.post('/negociacoes',negociacao).then(() => {
+            inputData.Value = '';
+            inputQuantidade.Value = 1;
+            inputValor.Value = 0.0;
+            inputData.focus();
+            alert('Negociação enviada com sucesso');
+        }).catch(erro => alert('Não foi possivel enviar a requisição'));
+
     }
 }
--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,5 +1,5 @@
-class HttpService{
-    get(url){
+class HttpService {
+    get(url) {
         return new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest();
             xhr.open("GET", url);
@@ -7,7 +7,7 @@ class HttpService{
             xhr.onreadystatechange = () => {
                 if (xhr.readyState == 4) {
                     if (xhr.status == 200) {
-                        
+
                         resolve(JSON.parse(xhr.responseText));
                     }
                     else {
@@ -18,5 +18,26 @@ class HttpService{
 
             xhr.send();
         });
+    }
+
+    post(url, dado) {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        console.log(xhr.responseText);
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+            xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
+        });
+
+
     }
 }
