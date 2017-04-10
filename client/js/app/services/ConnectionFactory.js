3402ddee2bc9535769020be58e062d825f8ca036
junin17 <carlosarju@gmail.com>
2017-04-10 01:49:38 -0300

    check

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,60 @@
+var ConnectionFactory = (function () {
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
+
+    var connection = null;
+
+    var close = null;
+
+    return class ConnectionFactory {
+        constructor() {
+            throw new Error("Não é possivel instanciar ConnectionFactory");
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                let request = window.indexedDB.open(dbName, version);
+
+                request.onupgradeneeded = event => {
+                    ConnectionFactory._createStores(event.target.result);
+                };
+
+                request.onsuccess = event => {
+                    
+                    if (!connection){
+                        connection = event.target.result
+                        close = connection.close.bind(connection);
+                        connection.close = function(){
+                            throw new Error("Não é possivel chamar este método");
+                        }
+                    } 
+                    resolve(connection);
+                };
+
+                request.onerror = event => {
+                    reject(event.target.error);
+                };
+            })
+        }
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+
+        static closeConnection(){
+            if (connection){
+                close();
+                connection = null;
+            }
+        }
+    }
+
+})();
+
